8a1dfa0e7d73451e1e023c38445d844330f75188
DenzelXIII <denzel_justin@hotmail.com>
2015-12-07 10:09:11 +0100

    Commit for adding the cubiquity and basic player files

---
+++ New Unity Project/Assets/_scripts/Player.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void FetchInput()
+    {
+        if (Input.GetKey(KeyCode.Joystick1Button0))
+        {
+            print("Pressing Square");
+
+        }
+
+        if (Input.GetKey(KeyCode.Joystick1Button1))
+        {
+            print("Pressing X");
+        }
+
+        if (Input.GetKey(KeyCode.Joystick1Button2))
+        {
+            print("Pressing Circle");
+        }
+
+        if (Input.GetKey(KeyCode.Joystick1Button3))
+        {
+            print("Pressing Triangle");
+        }
+
+        if (Input.GetKey(KeyCode.Joystick1Button4))
+        {
+            print("Pressing L1");
+        }
+
+        if (Input.GetKey(KeyCode.Joystick1Button5))
+        {
+            print("Pressing R1");
+        }
+    }
+	
+    void Movement()
+    {
+        float h = speed * Input.GetAxis("PS4_P1_R3");
+        Vector3 movementVector = new Vector3(0, 0, h);
+        transform.Translate(movementVector);
+        print(h);
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        FetchInput();
+        Movement();
+    }
+}
